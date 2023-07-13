0ac12b95071a1b72411e54395680d4571b712a79
Sarah Allateef <sara.korea96@gmail.com>
2023-07-13 11:05:38 +0300

    fix(page):deleting the console log from the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 
 export default function Home() {
-  console.log('hello Team');
+
   return (
     <main>
-      <Head>
-        <h1 className="test-hello">hello Team</h1>
-      </Head>
-    </main>
+    <Head>
+      <h1 className="test-hello">hello Team</h1>
+    </Head>
+  </main>
   )
 }
