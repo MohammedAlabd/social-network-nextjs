e590fd85c1466ac9d54447ef1defd94056a3dd29
Mahmood83367 <84744874+Mahmood83367@users.noreply.github.com>
2023-07-14 03:37:54 +0300

    making post body

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import demo from "../../public/profile.jpg";
 
 const ProfilePicture = ({ from } : { from: string }) => {
-  let dimensions;
+  let dimensions = 0;
 
   if (from === "likes") {
-    dimensions = 10;
+    dimensions = 15;
   } else if (from === "blog") {
     dimensions = 16;
   } else if (from === "profile") {
-    dimensions = 24;
+    dimensions = 30;
   }
 
   return (
-    <div className={`w-${dimensions} h-${dimensions} rounded-full overflow-hidden mr-3`}>
-      <Image className={`w-${dimensions} h-${dimensions}`} src={demo} alt="profilePicture"  />
+    <div className={`w-${dimensions} h-${dimensions} rounded-full overflow-hidden mr-7`}>
+      <Image width={dimensions*4} height={dimensions*4} src={demo} alt="profilePicture"  />
     </div>
   );
 };
