fc6f1c88cace45ecc62c7a60c11074217a56311b
ayaali333 <1aya.dn.ali@gmail.com>
2023-07-16 23:29:39 +0300

    add: add this images because we need it when the images come from APi we need just change the domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['i.pinimg.com'],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
