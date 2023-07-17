f5550c613a1008da4cc9883e0cebfb9313f0f5db
Sarah Allateef <sara.korea96@gmail.com>
2023-07-17 16:15:37 +0300

    style(texteditor styling) : css file for the text editor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+
+import '@/styles/globals.css'
+import '@/styles/tiptap.css'
+import type { AppProps } from 'next/app'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
