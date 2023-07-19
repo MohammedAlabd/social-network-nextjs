3a3232b8c44e5fe4198ae19ea49dab3d234c1251
Sarah Allateef <sara.korea96@gmail.com>
2023-07-19 18:38:07 +0300

    feat(i18next): adding the i18next configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { appWithTranslation } from 'next-i18next'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
