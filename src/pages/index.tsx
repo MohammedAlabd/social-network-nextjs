bd08617a8168e6d1b63b6f9939d6f90562fbe9ec
Sarah Allateef <sara.korea96@gmail.com>
2023-07-19 18:38:07 +0300

    feat(i18next): adding the i18next configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,34 @@
 import LoginBtn from '@/components/login-btn';
 import Head from 'next/head';
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
 
-export default function Home() {
+import { useTranslation, Trans } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+const Home = (
+  _props: InferGetStaticPropsType<typeof getStaticProps>
+) => {
+  const router = useRouter()
+  const { t, i18n } = useTranslation('common')
 
   return (
-
     <main>
-    <Head>
-      <h1 className="test-hello">hello Team</h1>
-    </Head>
-    <LoginBtn />
-  </main>
-  )
-
+      <Head>
+        <h1 className="test-hello">{t('hello Team')}</h1>
+      </Head>
+    </main>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async ({
+  locale,
+}) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? 'en', [
+      'common'
+    ])),
+  },
+})
+
+export default Home
