e567aae3a9f56abf843a1cc6c2b2a61dbc25aad1
Sarah Allateef <sara.korea96@gmail.com>
2023-07-24 11:32:08 +0300

    fix(i18next): fixed some unnecessary code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import Head from 'next/head';
-import type { GetStaticProps, InferGetStaticPropsType } from 'next';
+import type { GetStaticProps, InferGetStaticPropsType, GetStaticProps, InferGetStaticPropsType } from 'next';
 import '@/styles/globals.css';
-import { useRouter } from 'next/router';
 
-import { useTranslation, Trans } from 'next-i18next';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 function Home(_props: InferGetStaticPropsType<typeof getStaticProps>) {
-  const router = useRouter();
-  const { t, i18n } = useTranslation('common');
+  const { t } = useTranslation('common');
 
   return (
     <main>
       <Head>
         <h1 className="test-hello">{t('hello Team')}</h1>
+        <h1 className="test-hello">{t('hellopeople')}</h1>
       </Head>
     </main>
   );
