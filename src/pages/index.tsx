62024b6d89e7320d78d6233f83bdb4f3fb9a804e
Sarah Allateef <sara.korea96@gmail.com>
2023-07-24 11:32:08 +0300

    fix(i18next): fixed some unnecessary code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,22 @@ import LoginBtn from '@/components/login-btn';
 import Head from 'next/head';
 import '@/styles/globals.css';
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { useRouter } from 'next/router'
 
-import { useTranslation, Trans } from 'next-i18next'
+
+import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 const Home = (
   _props: InferGetStaticPropsType<typeof getStaticProps>
 ) => {
-  const router = useRouter()
-  const { t, i18n } = useTranslation('common')
 
+  const { t} = useTranslation('common')
+  
   return (
     <main>
       <Head>
-        <h1 className="test-hello">{t('hello Team')}</h1>
+      <h1 className="test-hello">{t('hello Team')}</h1>
+      <h1 className="test-hello">{t('hellopeople')}</h1>
       </Head>
     </main>
   );
