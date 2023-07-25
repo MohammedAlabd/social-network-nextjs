6f5151ffa8537cf9fc7d4d9ecc8b663fdf291550
Mahmood83367 <84744874+Mahmood83367@users.noreply.github.com>
2023-07-25 22:13:24 +0300

    install isomorphic-dompurify  and use it

--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import { sanitize } from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 
 function PostBody({ content }: { content: string }) {
-  const postContent = <div dangerouslySetInnerHTML={{ __html: content }} />;
-  //   const clean = sanitize(content, {
-  //     ALLOWED_TAGS: ['p', 'img', 'h1'],
-  //     ALLOWED_ATTR: ['className', 'dir'],
-  //     SAFE_FOR_TEMPLATES: true,
-  //     USE_PROFILES: { html: true },
-  //   });
-  return <div className="ml-3 mr-3 mt-10">{postContent}</div>;
+  const clean = DOMPurify.sanitize(content, {
+    // ALLOWED_TAGS: ['p', 'h1'],
+    // ALLOWED_ATTR: ['className', 'dir'],
+    // SAFE_FOR_TEMPLATES: true,
+    // USE_PROFILES: { html: true },
+  });
+  return (
+    <div className="ml-3 mr-3 mt-10">
+      <div dangerouslySetInnerHTML={{ __html: clean }} />
+    </div>
+  );
 }
 
 export default PostBody;
