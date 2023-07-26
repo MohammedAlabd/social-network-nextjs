342f264d00f92edc22b5080beb67976243a10479
Sarah Allateef <sara.korea96@gmail.com>
2023-07-26 14:53:39 +0300

    fix(textEditor): review fix 1.working on the handleSubmit function 2.adding DOMPurify for the handleSubmit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import {  useState } from 'react'
 
 import RichTextEditor from '../components/RichTextEditor'
+type Props = {
+    handleSubmit: (content: string) => void 
+    };
+export default function Home<Props>() {
+    const [editorContent, setEditorContent ] = useState('');
 
-export default function Home() {
+    const handleSubmit = (htmlContent:string) => {
+        setEditorContent(htmlContent);
+        //console.log('Content submitted:', htmlContent);
+      }
+    
+    
     return (
-         <RichTextEditor />
+         <RichTextEditor handleSubmit={handleSubmit}/>
     )
 }
 
