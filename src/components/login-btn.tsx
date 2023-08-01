a9bc859e07515f887726cf402387c7f41fc25a65
mayceAlazzawi <maynab4@gmail.com>
2023-08-01 12:54:44 +0300

    (feat): Adding login btn component helps test auth and will be edited to be a moe featured component later

---
+++ src/components/login-btn.tsx
@@ -0,0 +1,20 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function LoginBtn() {
+  const { data: session } = useSession()
+  if (session) {
+    return ( 
+      <>
+      Signed in as   <br />
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+    )
+  } else {
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
+}
