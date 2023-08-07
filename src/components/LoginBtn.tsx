8cc30e6a4047b887925541d9f732f6f084f1a8a7
mayceAlazzawi <maynab4@gmail.com>
2023-08-07 20:38:26 +0300

    Editing loginBtn so it I can put it in pofile menu

---
+++ src/components/LoginBtn.tsx
@@ -0,0 +1,19 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function LoginBtn() {
+  const { data: session } = useSession()
+  if (session) {
+    return ( 
+      <>
+            <button className="block px-4 py-2 text-sm text-gray-700" onClick={() => signOut()}>Sign out</button>
+
+    </>
+    )
+  } else {
+  return (
+    <>
+      <button className="block px-4 py-2 text-sm text-gray-700" onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
+}
