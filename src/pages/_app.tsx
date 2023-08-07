84e5105c0ab3f42edc31bafb1d47bfac7e376709
mayceAlazzawi <maynab4@gmail.com>
2023-08-07 10:42:47 +0300

    Adding Auth config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import '@/styles/globals.css';
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session} >
       <Component {...pageProps}/>
     </SessionProvider>
   )
-} 
+}
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,12 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from 'next-auth/providers/credentials';
-
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string
       }),
       CredentialsProvider({
         name: "Credentials",
@@ -25,8 +23,7 @@ export const authOptions = {
             }
         },
         async authorize(credentials) {
-            const user = { id: "42", name: "Dave", password: "nextauth" }
-
+            const user = { id: "1", name: "Mays", password: "11" }
             if (credentials?.username === user.name && credentials?.password === user.password) {
                 return user
             } else {
@@ -35,6 +32,7 @@ export const authOptions = {
         }
     })
 ],
+
 }
 
 export default NextAuth(authOptions)
