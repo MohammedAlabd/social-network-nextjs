d25decf63c90c3a0df6fbf8165d10d8ae77c2f35
mayceAlazzawi <maynab4@gmail.com>
2023-08-08 09:21:31 +0300

    Fixing a bug on authdata type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head';
+import { useSession } from 'next-auth/react';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
+  const {status, data} = useSession();
 
   return (
     <main>
+      <Navbar authData={data} authStatus={status} />
     <Head>
-      <h1 className="test-hello">hello Team</h1>
+      {/* <h1 className="test-hello">hello Team</h1> */}
     </Head>
   </main>
   )
