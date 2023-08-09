f490652e5278a390a3be4564e13513e6feb46ce8
ayaali333 <1aya.dn.ali@gmail.com>
2023-08-09 22:50:35 +0300

    feat:import UserType

--- src/pages/post-feed/index.tsx
+++ src/pages/post-feed/index.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
 import CreatPost from './CreatPost';
-import Post from './Post';
+import Post from './Posts';
+import { UserType } from './usersDummyData';
 
 export default function Index() {
+  const user: UserType = {
+    id: 1,
+    userPhoto: 'https://i.pinimg.com/236x/da/c5/f8/dac5f861cf4a5d0cfbe8f310016bea28.jpg',
+  };
   return (
     <div>
-      <CreatPost />
+      <CreatPost user={user} />
       <Post />
     </div>
   );
