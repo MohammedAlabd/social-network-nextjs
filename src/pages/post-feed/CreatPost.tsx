84ff7df601fd4b666b7b566251a5808d47918a22
ayaali333 <1aya.dn.ali@gmail.com>
2023-08-09 22:21:18 +0300

    feat:add user object as a props and render bnch of JSX

--- src/pages/post-feed/CreatPost.tsx
+++ src/pages/post-feed/CreatPost.tsx
@@ -1,54 +1,47 @@
 /* eslint-disable no-shadow */
 import Image from 'next/image';
+import React from 'react';
+import { UserType } from './usersDummyData';
 
-export default function CreatPost() {
-  interface User {
-    id: number;
-    userPhoto: string;
-  }
+type Props = {
+  user: UserType;
+};
 
-  const userList: User[] = [
-    {
-      id: 1,
-      userPhoto: 'https://i.pinimg.com/236x/da/c5/f8/dac5f861cf4a5d0cfbe8f310016bea28.jpg',
-    },
-  ];
+export default function CreatPost({ user }: Props) {
   return (
     <>
       <div className="space-y-4">
-        {userList.map((user) => (
-          <div key={user.id} className="mx-20 rounded-lg bg-white  shadow">
-            <div className="flex items-center ">
-              <div className="p-4">
-                <Image
-                  src={user.userPhoto}
-                  alt="Landscape picture"
-                  width={800}
-                  height={500}
-                  className="mask mask-squircle w-14"
-                />
+        <div key={user.id} className="mx-20 rounded-lg bg-white  shadow">
+          <div className="flex items-center ">
+            <div className="p-4">
+              <Image
+                src={user.userPhoto}
+                alt="Landscape picture"
+                width={800}
+                height={500}
+                className="mask mask-squircle w-14"
+              />
+            </div>
+            <div className="px-24 text-center">
+              <div className=" flex flex-col px-24 align-top ">
+                <p className="">Lets GET STARTED WITH A QUESTION</p>
+                <h3 className="px-24">What Would you Like to find in this community?</h3>
               </div>
-              <div className="px-24 text-center">
-                <div className=" flex flex-col px-24 align-top ">
-                  <p className="px-24 ">Lets GET STARTED WITH A QUESTION</p>
-                  <h3 className="px-24">What Would you Like to find in this community?</h3>
-                </div>
+            </div>
+            <div>
+              <div className="flex flex-col">
+                <button type="button">
+                  <span className="#ef4444 px-19 text-6xl font-bold text-red-600">&times;</span>
+                </button>
               </div>
-              <div>
-                <div className="flex flex-col">
-                  <button type="button">
-                    <span className="#ef4444 px-19 text-6xl font-bold text-red-600">&times;</span>
-                  </button>
-                </div>
-                <div className="py-20">
-                  <button type="button" className="btn-warning btn">
-                    Button
-                  </button>
-                </div>
+              <div className="py-20">
+                <button type="button" className="btn-warning btn">
+                  Button
+                </button>
               </div>
             </div>
           </div>
-        ))}
+        </div>
       </div>
       ;
     </>
