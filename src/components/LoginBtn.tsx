e4df8fd145e7f286e33f611dc1f40964d50bf990
mayceAlazzawi <maynab4@gmail.com>
2023-08-11 22:04:27 +0300

    tanslating the loginbtn

--- src/components/LoginBtn.tsx
+++ src/components/LoginBtn.tsx
@@ -1,20 +1,23 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+import { useTranslation } from 'next-i18next';
 
 export default function LoginBtn() {
   const { data: session } = useSession();
+  const { t } = useTranslation("navbar");
+
   if (session) {
     return (
       <>
-        <button className="block px-4 py-2 text-sm text-gray-700" onClick={() => signOut()}>
-          Sign out
+        <button className="block py-2 text-sm justify-between" onClick={() => signOut()}>
+         {t("Sign out")}
         </button>
       </>
     );
   } else {
     return (
       <>
-        <button className="block px-4 py-2 text-sm text-gray-700" onClick={() => signIn()}>
-          Sign in
+        <button className="block py-2 text-smjustify-between" onClick={() => signIn()}>
+        {t("Sign in")}
         </button>
       </>
     );
