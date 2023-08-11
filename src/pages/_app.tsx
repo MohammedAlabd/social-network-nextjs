e6086078672ed5cd78bd6b732981075b352b1df5
mayceAlazzawi <maynab4@gmail.com>
2023-08-11 22:12:51 +0300

    new banch fo building auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { SessionProvider } from 'next-auth/react';
+import '@/styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <SessionProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,24 @@
 import Head from 'next/head';
-import '@/styles/globals.css';
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Navbar from '@/components/Navbar';
-import { useSession } from 'next-auth/react';
-
 const Home = function (_props: InferGetStaticPropsType<typeof getStaticProps>) {
   const { t } = useTranslation('common');
-  const {status, data} = useSession();
+ 
   return (
     <main>
-      <Navbar authData={data} authStatus={status} />
+      <Navbar />
       <Head>
-        <h1 className="test-hello">{t('hello Team')}</h1>
-        <h1 className="test-hello">{t('hellopeople')}</h1>
+        {/* <h1 className="test-hello">{t('hello Team')}</h1>
+        <h1 className="test-hello">m</h1> */}
       </Head>
     </main>
   );
-  }
+};
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
-    ...(await serverSideTranslations(locale ?? 'en', ['common'])),
+    ...(await serverSideTranslations(locale ?? 'en', ['common', 'navbar'])),
   },
 });
 
