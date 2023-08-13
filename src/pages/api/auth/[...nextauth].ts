facb57c73f1108f24386e2eb5f768272916162bc
mayceAlazzawi <maynab4@gmail.com>
2023-08-13 11:17:00 +0300

    Adding an error if Google credentails ae not entered

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,39 +1,43 @@
-import NextAuth, { Session } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions = {
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_ID as string,
-        clientSecret: process.env.GOOGLE_SECRET as string
-      }),
-      CredentialsProvider({
-        name: "Credentials",
-        credentials: {
-            username: {
-                label: "Username:",
-                type: "text",
-                placeholder: "your-cool-username"
-            },
-            password: {
-                label: "Password:",
-                type: "password",
-                placeholder: "your-awesome-password"
-            }
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username:',
+          type: 'text',
+          placeholder: 'your-cool-username',
         },
-        async authorize(credentials) {
-            //Use obj will be delete once we have a BE
-            const user = { id: "1", name: "Mays", password: "11"}
-            if (credentials?.username === user.name && credentials?.password === user.password) {
-                return user
-            } else {
-                return null
-            }
+        password: {
+          label: 'Password:',
+          type: 'password',
+          placeholder: 'your-awesome-password',
+        },
+      },
+      async authorize(credentials) {
+        // Use obj will be deleted once we have a BE
+        const user = { id: '1', name: 'Mays', password: '11' };
+        if (credentials?.username === user.name && credentials?.password === user.password) {
+          return user;
+        } else {
+          return null;
         }
-    })
-],
+      },
+    }),
+  ],
+};
 
+// Custom error handling for Google credentials
+if (!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET) {
+  throw new Error('Google credentials are missing.');
 }
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
