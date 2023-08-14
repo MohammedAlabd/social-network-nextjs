bd20747cfef2d7beeb8a8527968925e66915be31
mayceAlazzawi <maynab4@gmail.com>
2023-08-14 11:44:12 +0300

    Deleting things that ae not elated to navba

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,43 +1,22 @@
 import LoginBtn from './LoginBtn';
 import { useTranslation } from 'next-i18next';
-import type { GetStaticProps, InferGetStaticPropsType } from 'next';
+import type { InferGetStaticPropsType } from 'next';
 import { getStaticProps } from '@/pages';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Image from 'next/image';
+import Bell from '../../public/media/icons/Bell.svg';
 
- //lines below will be needed fo authentication
-// interface _Props {
-//   authData: {
-//     user?: {
-//       name?: string | null | undefined;
-//       email?: string | null | undefined;
-//       image?: string | null | undefined;
-//     };
-//   } | null;
-//   authStatus: 'authenticated' | 'loading' | 'unauthenticated';
-// }
-
-const Navbar = function (
-  _Props: InferGetStaticPropsType<typeof getStaticProps>,
-  //lines below will be needed fo authentication
-  // authData: {
-  //   user?: {
-  //     name?: string | null | undefined;
-  //     email?: string | null | undefined;
-  //     image?: string | null | undefined;
-  //   };
-  // } | null,
-  // authStatus?: 'authenticated' | 'loading' | 'unauthenticated' | 'undefined'
-) {
-  const { t } = useTranslation('navbar');
-
+const Navbar = function (_Props: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { t } = useTranslation('common');
+  const bell = require('../../public/media/icons/bell.svg') as any;
   return (
     <div className="navbar bg-[#abd9e1] px-2 shadow-xl">
       <div className="logo flex-1">
         <a className="btn-ghost btn text-xl normal-case">
-          <img
+          <Image
             alt="Journey الرحلة logo"
             src="https://media1-production-mightynetworks.imgix.net/asset/56053552/JOURNEYUNCROPPED2-28.png?ixlib=rails-4.2.0&amp;auto=format&amp;w=150&amp;h=100&amp;fit=crop&amp;impolicy=Avatar&amp;crop=faces"
             width="60"
+            height="60"
           />
         </a>
       </div>
@@ -45,40 +24,37 @@ const Navbar = function (
         <div className="bell dropdown-end dropdown">
           <label tabIndex={0} className="bell btn-ghost btn-circle btn">
             <div className="bell-num indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="25" height="25">
-                <g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3H4a4 4 0 0 0 2-3v-3a7 7 0 0 1 4-6"></path>
-                  <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
-                </g>
-              </svg>
+              <Image priority src={bell} alt="Follow us on Twitter" />
               <span className="badge badge-sm indicator-item">8</span>
             </div>
           </label>
           <div tabIndex={0} className="card dropdown-content card-compact z-[1] mt-3 w-52 bg-base-100 shadow">
             <div className="card-body">
-              <span className="text-m border-b p-2 font-normal">User {t('liked your post')}</span>
-              <button className="text-info">{t('See more')}</button> 
+              <span className="text-m border-b p-2 font-normal">User {t('navbar.liked your post')}</span>
+              <button className="text-info">{t('navbar.See more')}</button>
             </div>
           </div>
         </div>
         <div className="dropdown-end dropdown">
           <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
             <div className="w-10 rounded-full">
-              <img src="https://media1-production-mightynetworks.imgix.net/asset/56159004/IMG_6521_2.heic?ixlib=rails-4.2.0&fm=jpg&q=100&auto=format&w=2004&h=2004&fit=crop&crop=faces&impolicy=Avatar&mask=ellipse" />
+              <Image src="https://media1-production-mightynetworks.imgix.net/asset/56159004/IMG_6521_2.heic?ixlib=rails-4.2.0&fm=jpg&q=100&auto=format&w=2004&h=2004&fit=crop&crop=faces&impolicy=Avatar&mask=ellipse" alt="pofile" width="100" height="100" />
             </div>
           </label>
           <ul tabIndex={0} className="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow">
             <li>
-              <a className="justify-between">{t('Hello, ')} </a>
+              <a className="justify-between">{t('navbar.Hello, ')} </a>
             </li>
             <li>
-              <a className="justify-between">{t('Profile')}</a>
+              <a className="justify-between">{t('navbar.Profile')}</a>
             </li>
             <li>
-              <a>{t('Settings')}</a>
+              <a>{t('navbar.Settings')}</a>
             </li>
             <li>
-              <LoginBtn />
+            <button className="block py-2 text-sm justify-between">
+               {t("navbar.Sign out")}
+            </button>
             </li>
           </ul>
         </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
 export default appWithTranslation(MyApp);
