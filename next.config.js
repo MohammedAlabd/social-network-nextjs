f7718944478d3dc6b2c4f916aa8b55751ef71f7a
mayceAlazzawi <maynab4@gmail.com>
2023-08-14 11:44:58 +0300

    Adding image and svg config
    Adding Image and Svg to navba and links next.confiq

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   i18n,
+  images: {
+      domains: [
+          "media1-production-mightynetworks.imgix.net",
+      ],
+  }
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,37 +3,19 @@ import { useTranslation } from 'next-i18next';
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { getStaticProps } from '@/pages';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
-interface _Props {
-  authData: {
-    user?: {
-      name?: string | null | undefined;
-      email?: string | null | undefined;
-      image?: string | null | undefined;
-    };
-  } | null;
-  authStatus: 'authenticated' | 'loading' | 'unauthenticated';
-}
+import Image from 'next/image';
+import Bell from '../../public/media/icons/Bell.svg';
 
 const Navbar = function (
   _Props: InferGetStaticPropsType<typeof getStaticProps>,
-  //lines below will be needed fo authentication
-  // authData: {
-  //   user?: {
-  //     name?: string | null | undefined;
-  //     email?: string | null | undefined;
-  //     image?: string | null | undefined;
-  //   };
-  // } | null,
-  // authStatus?: 'authenticated' | 'loading' | 'unauthenticated' | 'undefined'
 ) {
-  const { t } = useTranslation('navbar');
+  const { t } = useTranslation('common');
 
   return (
     <div className="navbar bg-[#abd9e1] px-2 shadow-xl">
       <div className="logo flex-1">
         <a className="btn-ghost btn text-xl normal-case">
-          <img
+          <Image
             alt="Journey الرحلة logo"
             src="https://media1-production-mightynetworks.imgix.net/asset/56053552/JOURNEYUNCROPPED2-28.png?ixlib=rails-4.2.0&amp;auto=format&amp;w=150&amp;h=100&amp;fit=crop&amp;impolicy=Avatar&amp;crop=faces"
             width="60"
@@ -44,12 +26,7 @@ const Navbar = function (
         <div className="bell dropdown-end dropdown">
           <label tabIndex={0} className="bell btn-ghost btn-circle btn">
             <div className="bell-num indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="25" height="25">
-                <g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3H4a4 4 0 0 0 2-3v-3a7 7 0 0 1 4-6"></path>
-                  <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
-                </g>
-              </svg>
+              <Image priority src={Bell} alt="Follow us on Twitter" />
               <span className="badge badge-sm indicator-item">8</span>
             </div>
           </label>
@@ -63,7 +40,7 @@ const Navbar = function (
         <div className="dropdown-end dropdown">
           <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
             <div className="w-10 rounded-full">
-              <img src="https://media1-production-mightynetworks.imgix.net/asset/56159004/IMG_6521_2.heic?ixlib=rails-4.2.0&fm=jpg&q=100&auto=format&w=2004&h=2004&fit=crop&crop=faces&impolicy=Avatar&mask=ellipse" />
+              <Image src="https://media1-production-mightynetworks.imgix.net/asset/56159004/IMG_6521_2.heic?ixlib=rails-4.2.0&fm=jpg&q=100&auto=format&w=2004&h=2004&fit=crop&crop=faces&impolicy=Avatar&mask=ellipse" alt="pofile" width="100" height="100" />
             </div>
           </label>
           <ul tabIndex={0} className="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow">
