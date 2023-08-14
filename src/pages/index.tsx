7f91afe1938f604f735734206348f379cc4bd790
mayceAlazzawi <maynab4@gmail.com>
2023-08-14 11:44:35 +0300

    Mege fom staging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,26 @@
 import Head from 'next/head';
-import type { GetStaticProps, InferGetStaticPropsType, GetStaticProps, InferGetStaticPropsType } from 'next';
-import '@/styles/globals.css';
-
+import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Navbar from '@/components/Navbar';
 
-function Home(_props: InferGetStaticPropsType<typeof getStaticProps>) {
-  const { t } = useTranslation('common');
-
-  return (
-    <main>
-      <Head>
-        <h1 className="test-hello">{t('hello Team')}</h1>
-        <h1 className="test-hello">{t('hellopeople')}</h1>
-      </Head>
-    </main>
-  );
-}
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
     ...(await serverSideTranslations(locale ?? 'en', ['common'])),
   },
 });
+
+const Home = function (_props: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { t } = useTranslation('common');
+  return (
+    <main>
+      <Navbar />
+      <Head>
+        <h1 className="test-hello">{t('test.hello Team')}</h1>
+        <h1 className="test-hello">{t('test.hellopeople')}</h1>
+      </Head>
+    </main>
+  );
+};
 
 export default Home;
