b253f83459b71e819c2f7535b29ea25e2f06c010
ayaali333 <1aya.dn.ali@gmail.com>
2023-08-18 00:22:07 +0300

    feat: added i18n from rebase main

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.pinimg.com'],
   },
+  i18n,
 };
 module.exports = nextConfig;
