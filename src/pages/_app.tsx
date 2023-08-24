6b87de04c8ccc89790851b272d510a1c61388c45
maycealazzawi <maynab4@gmail.com>
2023-08-24 15:43:44 +0300

    Fix(Auth): Changing the place of the session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import '@/styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <SessionProvider>
-    <Component {...pageProps} />
-  </SessionProvider>
+  <Component {...pageProps} />
 );
 
 export default appWithTranslation(MyApp);
